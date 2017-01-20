8a564066cf3fcc35a4bd7b52b550b6a1c990d003
osaizar <oierlas@gmail.com>
2017-01-20 18:18:23 +0100

    step2

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
-displayView = function(){
- // the code required to display a view
+var welcome = "welcomeview";
+var profile = "profileview";
+
+displayView = function(viewId){
+ document.getElementById("innerDiv").innerHTML = document.getElementById(viewId).innerHTML;
 };
 
 window.onload = function(){
  //code that is executed as the page is loaded.
  //You shall put your own custom code here.
  //window.alert() is not allowed to be used in your implementation.
- window.alert("Hello TDDD97!");
+ displayView(welcome);
 };
