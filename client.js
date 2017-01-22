eb55f87bbed2d2671fb7871b538a7dbeca0b9292
osaizar <oierlas@gmail.com>
2017-01-22 17:49:11 +0100

    Password checks

--- client.js
+++ client.js
@@ -12,31 +12,14 @@ displayView = function(){
 };
 
 window.onload = function(){
- //code that is executed as the page is loaded.
- //You shall put your own custom code here.
- //window.alert() is not allowed to be used in your implementation.
  displayView();
 };
 
 
-function validateLoginForm(){
-
-  var accepted = true;
-
+function logIn(){
   var email = document.forms["loginForm"]["email"].value;
   var password = document.forms["loginForm"]["password"].value;
 
-  if(email == "" || password == ""){
-      alert("fill the blanks!"); //test
-      accepted = false;
-  }
-
-  if (accepted){
-    logIn(email, password);
-  }
-}
-
-function logIn(email, password){
   var msg = serverstub.signIn(email, password);
 
   if (msg.success){ //debug
@@ -50,43 +33,28 @@ function logIn(email, password){
   displayView();
 }
 
-function validateSignUpForm(){
 
-  var accepted = true;
+function checkPasswords(){
+  var password = document.getElementById("s_password");
+  var rpassword = document.getElementById("s_rpassword");
 
-  var name = document.forms["signupForm"]["name"].value;
-  var fname = document.forms["signupForm"]["f-name"].value;
+  if(password.value != rpassword.value){
+    rpassword.setCustomValidity("Passwords do not match!");
+  }else{
+    rpassword.setCustomValidity('');
+  }
+}
+
+function signUp(){
+
+  var firstname = document.forms["signupForm"]["name"].value;
+  var familyname = document.forms["signupForm"]["f-name"].value;
   var gender_select = document.getElementById("gender");
   var gender = gender_select.options[gender_select.selectedIndex].text;
   var city = document.forms["signupForm"]["city"].value;
   var country = document.forms["signupForm"]["country"].value;
   var email = document.forms["signupForm"]["email"].value;
   var password = document.forms["signupForm"]["password"].value;
-  var rpassword = document.forms["signupForm"]["Rpassword"].value;
-
-  if(email == "" || password == "" || name == "" || fname == "" || gender == "" || city == "" || country == "" || rpassword == ""){
-      alert("fill the blanks!"); //test
-      accepted = false;
-  }
-
-  if(password != rpassword){
-      alert("The passwords do not match");
-      accepted = false;
-  }
-
-  if(password.length < 6){
-      alert("The password is too short");
-      accepted = false;
-  }
-
-  if(accepted){
-    signUp(email, password, name, fname, gender, city, country);
-  }
-
-  return accepted;
-}
-
-function signUp(email, password, firstname, familyname, gender, city, country){
 
   var user = {
     email: email,
